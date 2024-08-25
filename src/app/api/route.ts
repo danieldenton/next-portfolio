2f36dd9e0c78c79f8ed48ef72e5960a04a2961c5
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-25 07:36:58 -0700

    updated POST email

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,22 +1,22 @@
-import { Resend } from 'resend';
-import { EmailTemplate } from '@/app/components/email-template';
+import { Resend } from "resend";
+import { EmailTemplate } from "@/app/components/email-template";
 
 const resend = new Resend(process.env.resendKey);
 
 interface IProps {
-  name: string
-  email: string
-  message: string
+  name: string;
+  email: string;
+  message: string;
 }
 
 export async function POST(props: IProps) {
-  const { name, email, message } = props
+  const { name, email, message } = props;
   try {
     const { data, error } = await resend.emails.send({
       from: email,
-      to: ['danieldentondev@gmail.com'],
-      subject: 'Hello world',
-      html: `from: ${name} ${email}`,
+      to: ["danieldentondev@gmail.com"],
+      subject: "PORTFOLIO MESSAGE",
+      html: `from: ${name}<br>${email}`,
     });
 
     if (error) {
