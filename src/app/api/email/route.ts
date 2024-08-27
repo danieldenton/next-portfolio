d0fd1802e7b7a43735c182b1a9b7f508c8cb4ad9
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-27 10:57:09 -0700

    prayt

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,17 +1,14 @@
 import { Resend } from "resend";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
 const resend = new Resend(process.env.resendKey);
-const allowedDomains = ["https://danieldentondev.com", "https://www.danieldentondev.com"]
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    allowedDomains
-  );
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  const { name, email, message } = req.body;
+export async function POST(req: NextRequest) {
+  const res = NextResponse.next(); // Create a response object for setting headers
+  res.headers.set("Access-Control-Allow-Origin", "https://danieldentondev.com")
+  res.headers.set("Access-Control-Allow-Methods", "POST");
+  res.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  const { name, email, message } = await req.json()
   try {
     const { data, error } = await resend.emails.send({
       from: "Daniel <danie@danieldentondev.com>",
