e010cf599169af7b2d52c3fe8723f5c81142d282
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-26 21:18:52 -0700

    break

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,25 +1,22 @@
 import { Resend } from "resend";
 import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
+const resend = new Resend(process.env.resendKey);
 
-
-export default async function POST(req: NextApiRequest) {
+export async function POST(req: NextApiRequest) {
   const { name, email, message } = req.body;
-  const resend = new Resend(process.env.resendKey);
-
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
+      from: "Acme <onboarding@resend.dev>",
       to: ["danieldentondev@gmail.com"],
       subject: `Portfolio Message from ${name}`,
       html: `<p><strong>Name:</strong> ${name}</p><p><strong>Email:</strong> ${email}</p><p><strong>Message:</strong><br>${message}</p>`,
     });
 
     if (error) {
-      return NextResponse.json({ error }, { status: 500 });
+      return Response.json({ error }, { status: 500 });
     }
 
-    return NextResponse.json(data);
+    return Response.json(data);
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
