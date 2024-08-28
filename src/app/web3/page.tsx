71fcf7c56655325310c92084df20b5fc06229c99
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-28 10:48:28 -0700

    deleted commented out line

--- src/app/web3/page.tsx
+++ src/app/web3/page.tsx
@@ -37,7 +37,6 @@ export default function Web3() {
   });
 
   return (
-    // <div className="animate-fade sm:mx-20 mx-0 flex sm:flex-row flex-col flex-wrap border">
     <div className="flex flex-wrap justify-around sm:mx-20 m-0 flex sm:flex-row flex-col my-10 overflow-hidden">
       {projectDivs}
     </div>
