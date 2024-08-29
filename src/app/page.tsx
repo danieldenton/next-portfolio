ae78271ebabfe1732aa413a8db756b3a852180ea
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-28 21:47:45 -0700

    styling done for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from "./components/contact-form";
 
 export default function Home() {
   return (
-    <div className="sm:mx-20 flex sm:flex-row justify-around flex-col">
+    <div className="sm:mx-20 flex justify-around flex-col">
     <ContactForm />
     <p className="text-center mt-10">This app was created with Next.js</p>
     <p className="text-center my-2">DD 2024</p>
