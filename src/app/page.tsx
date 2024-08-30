4d0d224c0446cd8f348596833b4cfe769f704194
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-30 15:01:38 -0700

    mt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="sm:mx-20 flex justify-around flex-col overflow-hidden">
     <ContactForm />
-    <p className="text-center my-2">DD 2024</p>
+    <p className="text-center my-2 mt-10">DD 2024</p>
     </div>
   );
 }
