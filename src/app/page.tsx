5d28d214a1be0ab4ecf83bfc8f3d8a553cc2cb45
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-30 14:58:55 -0700

    removal of next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className="sm:mx-20 flex justify-around flex-col overflow-hidden">
     <ContactForm />
-    <p className="text-center mt-10">This app was created with Next.js</p>
     <p className="text-center my-2">DD 2024</p>
     </div>
   );
