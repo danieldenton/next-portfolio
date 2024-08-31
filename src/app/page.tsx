6e2dbca5d25adaa405789d0f64d1b256b6f1db1f
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-31 08:55:29 -0700

    metadata update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import { Metadata } from "next";
 import ContactForm from "./components/contact-form";
 
 export const metadata: Metadata = {
-  title: 'Contact',
+  title: "Contact | Daniel Denton Dev",
 };
 
 export default function Home() {
   return (
     <div className="animate-fade flex justify-center flex-col w-full overflow-hidden">
       <div className="flex justify-center sm:mb-[150px]">
-      <ContactForm />
+        <ContactForm />
       </div>
     </div>
   );
