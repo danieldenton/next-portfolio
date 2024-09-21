2ff76bb7645e0fb3e877aa1a85ff955e90fcaac4
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-21 15:47:23 -0700

    removal of second modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import ContactForm from "./components/contact-form";
-import Modal from "./components/modal";
 
 export default function Home() {
   return (
     <div className="animate-grow-fade animate-grow flex justify-center flex-col w-full overflow-hidden">
       <div className="flex flex-col justify-center items-center sm:mb-[150px]">
-        <Modal />
         <ContactForm />
       </div>
     </div>
