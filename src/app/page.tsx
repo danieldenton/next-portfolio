dfcc6affe5baecaf3d3cc55d906d0fd8bbeea6d7
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-21 15:49:04 -0700

    hopefully this fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from "./components/contact-form";
 
 export default function Home() {
   return (
-    <div className="animate-grow-fade animate-grow flex justify-center flex-col w-full overflow-hidden">
+    <div className="animate-grow-fade animate-grow flex flex-col w-full overflow-hidden">
       <div className="flex flex-col justify-center items-center sm:mb-[150px]">
         <ContactForm />
       </div>
