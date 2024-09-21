10250dc4a413360afb4a9051107f335506d38709
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-21 15:46:32 -0700

    removal of ModalContact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import ModalContact from "./components/modal-contact";
+import ContactForm from "./components/contact-form";
+import Modal from "./components/modal";
 
 export default function Home() {
   return (
     <div className="animate-grow-fade animate-grow flex justify-center flex-col w-full overflow-hidden">
       <div className="flex flex-col justify-center items-center sm:mb-[150px]">
-        <ModalContact />
+        <Modal />
+        <ContactForm />
       </div>
     </div>
   );
